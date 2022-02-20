cf98a9122516cb997c19e8e02fd2c49c69b5d8fd
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:25:35 +0000

    Remove controller comments

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
 
-  # GET /items
   def index
     @q = Item.ransack(params[:q])
     @items = @q.result(distinct: true).includes(:user, :messages,
@@ -9,24 +8,19 @@ class ItemsController < ApplicationController
     @location_hash = Gmaps4rails.build_markers(@items.where.not(location_latitude: nil)) do |item, marker|
       marker.lat item.location_latitude
       marker.lng item.location_longitude
-      marker.infowindow "<h5><a href='/items/#{item.id}'>#{item.title}</a></h5><small>#{item.location_formatted_address}</small>"
     end
   end
 
-  # GET /items/1
   def show
     @message = Message.new
   end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit; end
 
-  # POST /items
   def create
     @item = Item.new(item_params)
 
@@ -42,7 +36,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1
   def update
     if @item.update(item_params)
       redirect_to @item, notice: "Item was successfully updated."
@@ -51,7 +44,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
   def destroy
     @item.destroy
     message = "Item was successfully deleted."
@@ -64,12 +56,10 @@ class ItemsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_item
     @item = Item.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def item_params
     params.require(:item).permit(:title, :description, :user_id, :photo,
                                  :location, :category_id, :price, :status)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,23 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users
   def index
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).includes(:items,
                                                 :messages).page(params[:page]).per(10)
   end
 
-  # GET /users/1
   def show
     @message = Message.new
     @item = Item.new
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit; end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -33,7 +28,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       redirect_to @user, notice: "User was successfully updated."
@@ -42,7 +36,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     redirect_to users_url, notice: "User was successfully destroyed."
@@ -50,12 +43,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
