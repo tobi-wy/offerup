cfde036b73dc170ca75beec2354cdc4f95b7fd23
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:24:56 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,13 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :username, :string
+  attribute :email, :string
+  attribute :password, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
