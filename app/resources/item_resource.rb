b028a4c64e8cc492ede7b787460be3d06f1c2561
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:24:58 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,18 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :description, :string
+  attribute :user_id, :integer
+  attribute :photo, :string
+  attribute :location, :string
+  attribute :category_id, :integer
+  attribute :price, :integer
+  attribute :status, :boolean
+
+  # Direct associations
+
+  # Indirect associations
+
+end
