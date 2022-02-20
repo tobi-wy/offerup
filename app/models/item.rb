bd939616cdb3008e8e6414b9785b95c79962d841
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:24:04 +0000

    Direct association between categories and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   # Direct associations
 
+  belongs_to :category
+
   has_many   :messages,
              :dependent => :destroy
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Item, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:category) }
+
     it { should have_many(:messages) }
 
     it { should belong_to(:user) }
