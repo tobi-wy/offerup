34dbea8b17e26aae30c384747d63807bd95ad341
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:24:15 +0000

    Whitelist columns in Message for ActiveAdmin

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Message do
 
+ permit_params :sender_id, :recipient_id, :text, :item_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
